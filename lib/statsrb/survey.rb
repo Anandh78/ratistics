4ad09b3d7552113c35418c820472549b2da8bd48
Jerry D'Antonio <stumpjumper@gmail.com>
2013-01-21 16:32:53 -0500

    Survey reader assumes .gz file.

--- lib/statsrb/survey.rb
+++ lib/statsrb/survey.rb
@@ -2,37 +2,23 @@ module Statsrb
   module Survey
     extend self
 
-    FEMRESP = File.expand_path(File.join(File.dirname(__FILE__), '../../data', '2002FemResp.dat'))
-    FEMPREG = File.expand_path(File.join(File.dirname(__FILE__), '../../data', '2002FemPreg.dat'))
+    FEMRESP = File.expand_path(File.join(File.dirname(__FILE__), '../../data', '2002FemResp.dat.gz'))
+    FEMPREG = File.expand_path(File.join(File.dirname(__FILE__), '../../data', '2002FemPreg.dat.gz'))
 
     def read
 
-      ## open the femals respondents file
-      #count = 0
-      #File.foreach(FEMRESP) do |s|
-        #count = count + 1
-      #end
-      #puts "Number of respondents: #{count}"
-
-      # open the femals respondents file
+      # open the femaile respondents file
       count = 0
-      Zlib::GzipReader.open(Statsrb::Survey::FEMRESP + '.gz') do |gz|
+      Zlib::GzipReader.open(Statsrb::Survey::FEMRESP) do |gz|
         gz.each_line do |line|
           count = count + 1
         end
       end
       puts "Number of respondents: #{count}"
 
-      ## open the femals pregnancies file
-      #count = 0
-      #File.foreach(FEMPREG) do |s|
-        #count = count + 1
-      #end
-      #puts "Number of pregnancies: #{count}"
-
-      # open the femals pregnancies file
+      # open the femaile pregnancies file
       count = 0
-      Zlib::GzipReader.open(Statsrb::Survey::FEMPREG + '.gz') do |gz|
+      Zlib::GzipReader.open(Statsrb::Survey::FEMPREG) do |gz|
         gz.each_line do |line|
           count = count + 1
         end
