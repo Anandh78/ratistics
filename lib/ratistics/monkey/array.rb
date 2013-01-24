6334b9ef1ef55ae875efbe2741e5c316b529173e
Jerry D'Antonio <stumpjumper@gmail.com>
2013-01-24 12:53:16 -0500

    Added minimal YARD docs to monket-patched classes

--- lib/ratistics/monkey/array.rb
+++ lib/ratistics/monkey/array.rb
@@ -4,6 +4,7 @@ module Ratistics
 
   class ::Array
 
+    # @see Average::mean
     def mean(&block)
       return Ratistics.mean(self, &block)
     end
@@ -11,24 +12,29 @@ module Ratistics
     alias :avg :mean
     alias :average :mean
 
+    # @see Average::truncated_mean
     def truncated_mean(truncation, sorted=false, &block)
       return Ratistics.truncated_mean(self, truncation, sorted, &block)
     end
 
+    # @see Average::median
     def median(&block)
       return Ratistics.median(self, false, &block)
     end
 
+    # @see Average::mode
     def mode(&block)
       return Ratistics.mode(self, &block)
     end
 
+    # @see Distribution::variance
     def variance(mu=nil, &block)
       return Ratistics.variance(self, mu, &block)
     end
 
     alias :var :variance
 
+    # @see Distribution::standard_deviation
     def standard_deviation(mu=nil, &block)
       return Ratistics.standard_deviation(self, mu, &block)
     end
@@ -36,26 +42,31 @@ module Ratistics
     alias :std_dev :standard_deviation
     alias :stddev :standard_deviation
 
+    # @see Distribution::range
     def range(&block)
       return Ratistics.range(self, false, &block)
     end
 
+    # @see ProbabilityMassFunction::frequency
     def frequency(&block)
       return Ratistics.frequency(self, &block)
     end
 
+    # @see ProbabilityMassFunction::probability
     def probability(&block)
       return Ratistics.probability(self, &block)
     end
 
     alias :pmf :probability
 
+    # @see ProbabilityMassFunction::probability_mean
     def probability_mean(&block)
       return Ratistics.probability_mean(self, &block)
     end
 
     alias :pmf_mean :probability_mean
 
+    # @see ProbabilityMassFunction::probability_variance
     def probability_variance(&block)
       return Ratistics.probability_variance(self, &block)
     end
