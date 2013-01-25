8d9c96ee39b105debcbece8aa9dd30b0a5488136
Jerry D'Antonio <stumpjumper@gmail.com>
2013-01-25 15:57:52 -0500

    Added more test data.

--- data/survey.rb
+++ data/survey.rb
@@ -1,11 +1,18 @@
 require 'zlib'
 require 'hamster/vector'
+require 'csv'
 
 module Survey
   extend self
 
-  FEMRESP = File.expand_path(File.join(File.dirname(__FILE__), '2002FemResp.dat.gz'))
-  FEMPREG = File.expand_path(File.join(File.dirname(__FILE__), '2002FemPreg.dat.gz'))
+  FEMRESP_RAW = File.expand_path(File.join(File.dirname(__FILE__), '2002FemResp.dat.gz'))
+  FEMRESP_CSV = File.expand_path(File.join(File.dirname(__FILE__), '2002FemResp.csv'))
+
+  FEMPREG_RAW = File.expand_path(File.join(File.dirname(__FILE__), '2002FemPreg.dat.gz'))
+  FEMPREG_CSV = File.expand_path(File.join(File.dirname(__FILE__), '2002FemPreg.csv'))
+
+  RACE_RAW = File.expand_path(File.join(File.dirname(__FILE__), 'race.dat.gz'))
+  RACE_CSV = File.expand_path(File.join(File.dirname(__FILE__), 'race.csv'))
 
   RESPONDENT_FIELDS = [
     [:caseid, 1, 12, :to_i].freeze,
@@ -24,20 +31,53 @@ module Survey
     [:finalwgt, 423, 440, :to_f].freeze,
   ].freeze
 
+  RACE_FIELDS = [
+    [:place, 1, 6, :to_i].freeze,
+    [:div_tot, 7, 15, :strip].freeze,
+    [:string, 16, 21, :strip].freeze,
+    [:guntime, 22, 29, :strip].freeze,
+    [:nettime, 30, 38, :strip].freeze,
+    [:pace, 39, 44, :strip].freeze,
+    [:name, 45, 67, :strip].freeze,
+    [:age, 68, 70, :to_i].freeze,
+    [:gender, 71, 72, :strip].freeze,
+    [:race_num, 73, 78, :to_i].freeze,
+    [:location, 79, 101, :strip].freeze,
+  ].freeze
+
   def get_respondent_data
-    @@respondents ||= load_file(Survey::FEMRESP, Survey::RESPONDENT_FIELDS)
+    @@respondents ||= load_file(Survey::FEMRESP_RAW, Survey::RESPONDENT_FIELDS)
   end
 
   def get_pregnancy_data
-    @@pregnancies ||= load_file(Survey::FEMPREG, Survey::PREGNANCY_FIELDS)
+    @@pregnancies ||= load_file(Survey::FEMPREG_RAW, Survey::PREGNANCY_FIELDS)
+  end
+
+  def get_race_data
+    @@race ||= load_file(Survey::RACE_RAW, Survey::RACE_FIELDS)
+  end
+
+  def load_respondent_csv
+    @@respondents_csv ||= load_csv(Survey::FEMRESP_CSV, Survey::RESPONDENT_FIELDS)
+  end
+
+  def load_pregnancy_csv
+    @@pregnancies_csv ||= load_csv(Survey::FEMPREG_CSV, Survey::PREGNANCY_FIELDS)
+  end
+
+  def load_race_csv
+    @@race_csv ||= load_csv(Survey::RACE_CSV, Survey::RACE_FIELDS)
   end
 
   def get_counts
-    respondents = load_respondent_data
+    respondents = get_respondent_data
     puts "Number of respondents: #{respondents.count}"
 
-    pregnancies = load_pregnancy_data
+    pregnancies = get_pregnancy_data
     puts "Number of pregnancies: #{pregnancies.count}"
+
+    racers = get_race_data
+    puts "Number of racers: #{racers.count}"
   end
 
   def load_file(path, definition)
@@ -51,6 +91,20 @@ module Survey
         end
         data = data.cons(record.freeze)
       end
+    end
+
+    return data
+  end
+
+  def load_csv(path, definition)
+    data = Hamster.vector
+
+    CSV.foreach(path) do |row|
+      record = {}
+        definition.each_index do |index|
+          record[definition[index][0]] = row[index]
+        end
+      data = data.cons(record.freeze)
     end
 
     return data
