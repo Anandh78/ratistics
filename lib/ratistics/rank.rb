c5f4cd0b6186b3ef56f39f8ce65feaf888ae532a
Jerry D'Antonio <stumpjumper@gmail.com>
2013-02-04 15:58:37 -0500

    Removed unnecessary comments

--- lib/ratistics/rank.rb
+++ lib/ratistics/rank.rb
@@ -181,28 +181,11 @@ module Ratistics
     alias :percentile :linear_rank
     alias :centile :linear_rank
 
-    #def weighted_percentile(data, percentile, opts={}, &block)
-    #end
-
-    #def percentile?
-    #end
-
-    #alias :centile? :percentile?
-
-    ## The percentage of scores in the frequency distribution that are the same or lower
-    #def percentile_rank(data, value, sorted=false, opts={}, &block)
-    #alias :centile_rank :percentile_rank
-
-    #def percentile_rank?(data, value, percentile, sorted=false, opts={}, &block) 
-    #alias :centile_rank? :percentile_rank?
-
     #def lower_quartile
     #def upper_quartile
     #def first_quartile
     #def second_quartile
     #def third_quartile
-
-
 
   end
 end
