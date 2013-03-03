12df1a48618c75445d2968869a1c1288d31b2127
Jerry D'Antonio <stumpjumper@gmail.com>
2013-03-02 21:14:50 -0500

    Updated test scripts

--- install-all-rubies.sh
+++ install-all-rubies.sh
@@ -1,13 +1,13 @@
 #!/bin/bash --login
 
-rvm install ruby-2.0.0-p0
-rvm install ruby-1.9.3
-rvm install ruby-1.9.2
-rvm install ruby-1.8.7
-rvm install ree-1.8.7-2012.02
-rvm install rbx-head-d18
-rvm install rbx-head-d19
-rvm install jruby-1.7.0
-rvm install jruby-1.6.8
-rvm install jruby-1.6.7.2
-rvm install jruby-1.6.7
+rvm install ruby-2.0.0-p0 --verify-downloads 1
+rvm install ruby-1.9.3 --verify-downloads 1
+rvm install ruby-1.9.2 --verify-downloads 1
+rvm install ruby-1.8.7 --verify-downloads 1
+rvm install ree-1.8.7 --verify-downloads 1
+rvm install rbx-head-d18 --verify-downloads 1
+rvm install rbx-head-d19 --verify-downloads 1
+rvm install jruby-1.7.0 --verify-downloads 1
+rvm install jruby-1.6.8 --verify-downloads 1
+rvm install jruby-1.6.7.2 --verify-downloads 1
+rvm install jruby-1.6.7 --verify-downloads 1
--- test-all-rubies.sh
+++ test-all-rubies.sh
@@ -1,51 +1,26 @@
 #!/bin/bash --login
 
-rvm use ruby-1.8.7-p371@ratistics --create
-gem install bundler &>/dev/null 
-bundle install &>/dev/null
-rake spec
-
-rvm use ruby-1.9.2-p320@ratistics --create
-gem install bundler &>/dev/null 
-bundle install &>/dev/null
-rake spec
-
-rvm use ruby-1.9.3-p327@ratistics --create
-gem install bundler &>/dev/null 
-bundle install &>/dev/null
-rake spec
-
 rvm use ruby-2.0.0-p0@ratistics --create
 gem install bundler &>/dev/null 
 bundle install &>/dev/null
 rake spec
 
-rvm use jruby-1.6.7@ratistics --create
-JRUBY_OPTS="-Xcext.enabled=true" gem install bundler &>/dev/null 
-JRUBY_OPTS="-Xcext.enabled=true" bundle install &>/dev/null
-JRUBY_OPTS="-Xcext.enabled=true" rake spec
-
-rvm use jruby-1.6.7.2@ratistics --create
-JRUBY_OPTS="-Xcext.enabled=true" gem install bundler &>/dev/null 
-JRUBY_OPTS="-Xcext.enabled=true" bundle install &>/dev/null
-JRUBY_OPTS="-Xcext.enabled=true" rake spec
-
-rvm use jruby-1.6.8@ratistics --create
-JRUBY_OPTS="-Xcext.enabled=true" gem install bundler &>/dev/null 
-JRUBY_OPTS="-Xcext.enabled=true" bundle install &>/dev/null
-JRUBY_OPTS="-Xcext.enabled=true" rake spec
-
-rvm use jruby-1.7.0@ratistics --create
-JRUBY_OPTS="-Xcext.enabled=true" gem install bundler &>/dev/null 
-JRUBY_OPTS="-Xcext.enabled=true" bundle install &>/dev/null
-JRUBY_OPTS="-Xcext.enabled=true" rake spec
-
-rvm use ree-1.8.7-2012.02@ratistics --create
+rvm use ruby-1.9.3@ratistics --create
 gem install bundler &>/dev/null 
 bundle install &>/dev/null
 rake spec
 
-rvm use rbx-head-d18@ratistics --create
+rvm use ruby-1.9.2@ratistics --create
+gem install bundler &>/dev/null 
+bundle install &>/dev/null
+rake spec
+
+rvm use ruby-1.8.7@ratistics --create
+gem install bundler &>/dev/null 
+bundle install &>/dev/null
+rake spec
+
+rvm use ree-1.8.7@ratistics --create
 gem install bundler &>/dev/null 
 bundle install &>/dev/null
 rake spec
@@ -55,3 +30,30 @@ gem install bundler &>/dev/null
 bundle install &>/dev/null
 rake spec
 
+rvm use rbx-head-d18@ratistics --create
+gem install bundler &>/dev/null 
+bundle install &>/dev/null
+rake spec
+
+rvm use jruby-1.7.0@ratistics --create
+JRUBY_OPTS="-Xcext.enabled=true" gem install bundler &>/dev/null 
+JRUBY_OPTS="-Xcext.enabled=true" bundle install &>/dev/null
+JRUBY_OPTS="-Xcext.enabled=true" rake spec
+
+rvm use jruby-1.6.8@ratistics --create
+JRUBY_OPTS="-Xcext.enabled=true" gem install bundler &>/dev/null 
+JRUBY_OPTS="-Xcext.enabled=true" bundle install &>/dev/null
+JRUBY_OPTS="-Xcext.enabled=true" rake spec
+
+rvm use jruby-1.6.7.2@ratistics --create
+JRUBY_OPTS="-Xcext.enabled=true" gem install bundler &>/dev/null 
+JRUBY_OPTS="-Xcext.enabled=true" bundle install &>/dev/null
+JRUBY_OPTS="-Xcext.enabled=true" rake spec
+
+rvm use jruby-1.6.7@ratistics --create
+JRUBY_OPTS="-Xcext.enabled=true" gem install bundler &>/dev/null 
+JRUBY_OPTS="-Xcext.enabled=true" bundle install &>/dev/null
+JRUBY_OPTS="-Xcext.enabled=true" rake spec
+
+cd .
+bundle update
